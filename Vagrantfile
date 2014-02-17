9facd42bf47aa4dac3c41ca17ba615255a662714
ubergesundheit <ubergesundheit@googlemail.com>
2014-02-17 17:16:23 +0100

    runs tilemill and nginx

--- Vagrantfile
+++ Vagrantfile
@@ -7,9 +7,12 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "precise64_4"
 
-  config.vm.network :forwarded_port, guest: 80, host: 8080
+  #config.vm.network :forwarded_port, guest: 80, host: 8080
+  #config.vm.network :forwarded_port, guest: 5432, host: 5432
+  #config.vm.network :forwarded_port, guest: 20009, host: 20009
+  #config.vm.network :forwarded_port, guest: 20008, host: 20008
 
-  # config.vm.network :private_network, ip: "192.168.33.10"
+  config.vm.network :private_network, ip: "192.168.33.10"
 
   # config.ssh.forward_agent = true
 
---
+++ bootstrap.sh
@@ -0,0 +1,38 @@
+apt-get install --yes python-software-properties
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' >> /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+yes | apt-add-repository ppa:developmentseed/mapbox
+yes | apt-add-repository ppa:nginx/stable
+apt-get update --yes
+apt-get install --yes tilemill nginx postgresql-9.3-postgis-2.1 postgresql-contrib #fonts-cantarell lmodern ttf-aenigma ttf-georgewilliams ttf-bitstream-vera ttf-sjfonts ttf-tuffy tv-fonts ubuntustudio-font-meta
+
+# install adminpack for postgresql
+su -l -c "psql -c 'CREATE EXTENSION adminpack;'" - postgres
+
+#createuser -d -E -i -l -r -s tilemill
+
+tee /etc/nginx/sites-enabled/default <<FOF
+
+server {
+  listen 80;
+  server_name localhost;
+
+  location /tiles/ {
+    proxy_set_header Host \$http_host;
+    proxy_pass http://127.0.0.1:20008;
+  }
+
+  location / {
+    proxy_set_header Host \$http_host;
+    proxy_pass http://127.0.0.1:20009;
+    #auth_basic "Restricted";
+    #auth_basic_user_file htpasswd;
+  }
+}
+
+FOF
+
+
+service nginx restart
+
+start tilemill
