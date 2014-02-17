c8c48a15a46cffc8cdc0ba130f3d418b3b2e0873
ubergesundheit <ubergesundheit@googlemail.com>
2014-02-17 18:44:40 +0100

    create postgis template and install postgis

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,12 @@ export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
 sudo dpkg-reconfigure locales
 
+su -l -c "createdb -E UTF8 --locale=en_US.utf8 -T template0 template_postgis" - postgres
+
+su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis;'" - postgres
+su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis_topology;'" - postgres
+su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis_tiger_geocoder;'" - postgres
+
 # install adminpack for postgresql
 su -l -c "psql -c 'CREATE EXTENSION adminpack;'" - postgres
 
