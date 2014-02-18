78fc9996d705524a314c6d5c657012fbcaca0c9d
ubergesundheit <ubergesundheit@googlemail.com>
2014-02-18 12:19:25 +0100

    move data stuff into own file

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,25 +6,6 @@ yes | apt-add-repository ppa:nginx/stable
 apt-get update --yes
 apt-get install --yes tilemill nginx-light postgresql-9.3-postgis-2.1 postgresql-contrib #fonts-cantarell lmodern ttf-aenigma ttf-georgewilliams ttf-bitstream-vera ttf-sjfonts ttf-tuffy tv-fonts ubuntustudio-font-meta
 
-# sed -n '1{p;q}' /proc/meminfo
-
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen en_US.UTF-8
-sudo dpkg-reconfigure locales
-
-su -l -c "createdb -E UTF8 --locale=en_US.utf8 -T template0 template_postgis" - postgres
-
-su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis;'" - postgres
-su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis_topology;'" - postgres
-su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis_tiger_geocoder;'" - postgres
-
-# install adminpack for postgresql
-su -l -c "psql -c 'CREATE EXTENSION adminpack;'" - postgres
-
-#createuser -d -E -i -l -r -s tilemill
-
 tee /etc/nginx/sites-enabled/default <<FOF
 server {
   listen 80;
---
+++ prepare-data.sh
@@ -0,0 +1,18 @@
+# sed -n '1{p;q}' /proc/meminfo
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+sudo dpkg-reconfigure locales
+
+su -l -c "createdb -E UTF8 --locale=en_US.utf8 -T template0 template_postgis" - postgres
+
+su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis;'" - postgres
+su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis_topology;'" - postgres
+su -l -c "psql -d template_postgis -c 'CREATE EXTENSION postgis_tiger_geocoder;'" - postgres
+
+# install adminpack for postgresql
+su -l -c "psql -c 'CREATE EXTENSION adminpack;'" - postgres
+
+#createuser -d -E -i -l -r -s tilemill
